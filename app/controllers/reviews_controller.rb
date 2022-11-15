d55dcff681047d6ece90c6773d537b02f727bb47
Tony Wang <tony94042@gmail.com>
2022-11-15 13:55:39 +1100

    Fixed Syntax

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,7 +7,11 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
-    redirect_to restaurant_path(@restaurant)
+
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
   end
 
   private
